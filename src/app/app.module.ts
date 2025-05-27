bb60b16deb2a29bbfd82926cee34e78e6599519c
pailas02 <jesus.botero22412@ucaldas.edu.co>
2025-05-27 08:19:12 -0500

    esta madre esta sin oauth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app.routing';
@@ -18,8 +17,6 @@ import { ComponentsModule } from './components/components.module';
 // import { ManageComponent } from './pages/theaters/manage/manage.component'; // Ya declarado en TheatersModule
 import { ManageComponent } from './pages/obra-municipal/manage/manage.component'; // Asegúrate de que TheatersModule esté importado
 import { AuthInterceptor } from './interceptor/oauth.interceptor';
-import { NoAuthenticatedGuard } from './gurds/no-authenticated.guard';
-import { AuthenticatedGuard } from './gurds/authenticated.guard';
 
 @NgModule({
   imports: [
@@ -37,13 +34,7 @@ import { AuthenticatedGuard } from './gurds/authenticated.guard';
     AuthLayoutComponent,
   ],
   providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,
-    },
-    AuthenticatedGuard,
-    NoAuthenticatedGuard 
+   AuthInterceptor
   ],
   bootstrap: [AppComponent]
 })
--- src/app/layouts/auth-layout/auth-layout.routing.ts
+++ src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -2,9 +2,8 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from '../../pages/login/login.component';
 import { RegisterComponent } from '../../pages/register/register.component';
-import { NoAuthenticatedGuard } from 'src/app/gurds/no-authenticated.guard';
 
 export const AuthLayoutRoutes: Routes = [
-    { path: 'login', canActivate:[NoAuthenticatedGuard], component: LoginComponent },
-    { path: 'register', canActivate:[NoAuthenticatedGuard], component: RegisterComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent }
 ];
